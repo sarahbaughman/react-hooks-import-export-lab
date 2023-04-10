cbde0912baedb44a9f104878ba7f13d2721c1d07
Sarah Baughman <sarahbaughman92@gmail.com>
2023-04-10 18:07:10 -0400

    finished lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
 import React from "react";
+import Home from "./Home"
+import NavBar from "./NavBar"
+import About from "./About"
+// import {username, city, image} from "./data/user"
 
 function App() {
   return (
